47864756d5596067bd5ae2c9f7cf206270dcb098
bswarna04 <bswarna04@gmail.com>
2021-12-08 17:10:22 -0500

    FizzBuzz example

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num=100;
+        for (int i=1;i<=num;i++){
+           // if(i%3==0 && i%5==0){ or we can write 
+              if(i%15==0){
+                System.out.println("FizzBuzz"+" ");
+            }else if(i%3==0){
+                System.out.println("Fizz"+" ");
+            }else if(i%5==0){
+                System.out.println("Buzz"+" ");
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+}
